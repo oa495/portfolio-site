c032bd5247364f630bd4929cb2fc7865c2488d11
Omayeli Arenyeka <aomayeli@gmail.com>
2018-01-06 13:21:26 -0800

    Adding transitions: links and project images

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
 function addEventListeners() {
 	var projects = document.getElementsByClassName('projects')[0];
-
 	function toggleDisplay(e) {
+    var canvas = document.getElementsByClassName('canvas-container')[0];
 		var projectLink = e.target;
 		if (projectLink && projectLink.nodeName == 'A') {
 			var img = projectLink.parentElement.querySelector('.portfolio-img');
-			console.log(img);
 			if (img) {
-				img.classList.toggle('dn');
+				img.classList.toggle('display');
+        canvas.classList.toggle('dn');
 			}
 		}
 	}
